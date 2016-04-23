f16d903537ac964b752a307b1232ed34cfa0fbce
RobStallion <Robertefrancis18@gmail.com>
2016-04-23 18:08:27 +0100

    change all imports and exports in backend

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,28 +1,26 @@
 require('env2')('./config.env')
 
-import Hapi from 'hapi'
+const Hapi = require('hapi')
+const helper = require('./helpers/server-helpers.js')
 const server = new Hapi.Server()
 const port = process.env.PORT || 4000
 
-// helper methods
-import { handlePlugins, handleStart } from './helpers/server-helpers.js'
-
 // server plugins
-import Inert from 'inert'
+const Inert = require('inert')
 
 // server routes
-import Hello from './routes/Hello.js'
-import Images from './routes/Images.js'
-import ReactUrls from './routes/ReactUrls.js'
-import Scripts from './routes/Scripts.js'
+const Hello = require('./routes/Hello.js')
+const Images = require('./routes/Images.js')
+const ReactUrls = require('./routes/ReactUrls.js')
+const Scripts = require('./routes/Scripts.js')
 
 const ConnectionSettings = { port, routes: {cors: true} }
 const Plugins = [ Inert ]
 const Routes = [ Hello, Images, ReactUrls, Scripts ]
 
 server.connection(ConnectionSettings)
-server.register(Plugins, handlePlugins)
+server.register(Plugins, helper.handlePlugins)
 server.route(Routes)
-server.start(handleStart)
+server.start(helper.handleStart)
 
-export default server
+module.exports = port
