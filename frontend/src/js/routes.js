00397dcf549601c3c78a324f8410bc8744456703
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-04-23 16:39:55 +0100

    Add talk page

--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -5,11 +5,13 @@ import App from './components/App.js'
 import Home from './components/Home/index.js'
 import About from './components/About/index.js'
 import Contact from './components/Contact/index.js'
+import Talk from './components/Talk/index.js'
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={Home} />
     <Route path='/about' component={About} />
     <Route path='/contact' component={Contact} />
+    <Route path='/talk' component={Talk} />
   </Route>
 )
