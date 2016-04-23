a296cca5098e8155f35e3611ddff67366915693e
tas12 <tsultanah89@gmail.com>
2016-04-23 18:22:21 +0100

    styling form

--- frontend/src/js/components/Screening/Question.js
+++ frontend/src/js/components/Screening/Question.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import {Col, Row} from 'react-bootstrap'
 
 export default (props) => {
   return (
-    <div style={{display: 'inline'}}>
-      <h3>{props.question}</h3>
-      <label><input className="notTrue" type='radio' name={props.name} /></label>
-      <label><input className="somewhatTrue" type='radio' name={props.name} /></label>
-      <label><input className="certainlyTrue" type='radio' name={props.name} /></label>
+    <div>
+      <Row>
+        <Col md={6}>
+          <p style={{display: 'inline'}}>{props.question}</p>
+        </Col>
+        <Col md={2}>
+        <label><input className="notTrue" type='radio' name={props.name} /></label>
+        </Col>
+        <Col md={2}>
+        <label><input className="somewhatTrue" type='radio' name={props.name} /></label>
+        </Col>
+        <Col md={2}>
+        <label><input className="certainlyTrue" type='radio' name={props.name} /></label>
+        </Col>
+      </Row>
     </div>
   )
 }
