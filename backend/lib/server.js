ec13853885e340ed0ddabbdd322299779ea973b0
shouston3 <shouston3@me.com>
2016-04-24 12:33:44 +0100

    adding an endpoint and a button to reach it

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,7 +1,10 @@
 require('env2')('./config.env')
 
+var x = 5
+
 const Hapi = require('hapi')
 const helper = require('./helpers/server-helpers.js')
+const messagebird = require('messagebird')(process.env.API_KEY)
 const server = new Hapi.Server()
 const port = process.env.PORT || 4000
 
@@ -13,14 +16,43 @@ const Hello = require('./routes/Hello.js')
 const Images = require('./routes/Images.js')
 const ReactUrls = require('./routes/ReactUrls.js')
 const Scripts = require('./routes/Scripts.js')
+const MessageBird = {
+  path: '/sendText',
+  method: 'POST',
+  handler: (request, reply) => {
+    const params = {
+      originator: '+447860039046',
+      recipients: [
+      '00447590490239'
+      ],
+      body: 'Bazinga!!'
+    }
+    setTimeout(message, 30000)
+    function message() {
+      messagebird.messages.create(params, (err, response) => {
+       console.log(err ? err : response)
+      })
+    }
+    reply('BAZINGAAAA')
+  }
+}
 
 const ConnectionSettings = { port, routes: {cors: true} }
 const Plugins = [ Inert ]
-const Routes = [ Hello, Images, ReactUrls, Scripts ]
+const Routes = [ Hello, Images, MessageBird, Scripts, ReactUrls ]
 
 server.connection(ConnectionSettings)
 server.register(Plugins, helper.handlePlugins)
 server.route(Routes)
-server.start(helper.handleStart)
+
+const handleStart = (err) => {
+  if (err) {
+    console.log('server error: ', err)
+  } else {
+    console.log('server listening on port: ', port)
+  }
+}
+
+server.start(handleStart)
 
 module.exports = port
