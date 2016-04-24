d176cfcd8c09bdb5f7e9df4890d3f4ed63ba19ab
shouston3 <shouston3@me.com>
2016-04-24 14:51:23 +0100

    styled reminder page with a button and text describing what the page does

--- frontend/src/js/components/Remind/index.js
+++ frontend/src/js/components/Remind/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
 import axios from 'axios'
 
 export default class Refer extends Component {
@@ -16,7 +17,12 @@ export default class Refer extends Component {
   render () {
     return (
       <div>
-        <h1 onClick={this.sendMessage}>Message</h1>
+        <h3>Send a reminder</h3>
+        <h5>Send a reminder message to yourself or your specialist: </h5>
+        <p>
+          <span>Number to message: </span><input type='text'></input>
+        </p>
+        <Button onClick={this.sendMessage}>Message</Button>
       </div>
     )
   }
